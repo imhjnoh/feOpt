7731b1ec5be50acd3dfa5757c8d7477b8828de91
imhjnoh <imhjnoh@gmail.com>
2024-03-05 00:31:05 +0900

    #2: 컴포넌트 사전 로딩

--- lecture-2/src/App.js
+++ lecture-2/src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy, useState } from 'react'
+import React, { Suspense, lazy, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Header from './components/Header'
 import InfoTable from './components/InfoTable'
@@ -8,6 +8,10 @@ const LazyImageModal = lazy(() => import('./components/ImageModal'))
 
 function App() {
     const [showModal, setShowModal] = useState(false)
+
+    useEffect(() => {
+        const component = import('./components/ImageModal')
+    }, [])
 
     return (
         <div className="App">
